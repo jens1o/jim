66053004504a89ad0414f61feab28cc36f5e2919
Jens Hausdorf <mail@jens-hausdorf.de>
2022-08-21 20:34:02 +0200

    Use icons in navigation bar

--- jim-frontend/src/App.vue
+++ jim-frontend/src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import { useAppBackgroundStore } from "./stores/appBackground";
+import HomeIcon from "./components/icons/HomeIcon.vue";
+import PeopleIcon from "./components/icons/PeopleIcon.vue";
+import ClockIcon from "./components/icons/ClockIcon.vue";
+import CalendarIcon from "./components/icons/CalendarIcon.vue";
+import WalletIcon from "./components/icons/WalletIcon.vue";
+import CardIcon from "./components/icons/CardIcon.vue";
 
 const appBackground = useAppBackgroundStore();
 </script>
@@ -15,16 +21,28 @@ const appBackground = useAppBackgroundStore();
       </h1>
     </header>
 
-    <RouterView class="pb-20 max-w-screen-2xl mx-auto" />
+    <RouterView class="pb-15 max-w-screen-2xl mx-auto" />
 
-    <footer class="max-w-screen-2xl mx-auto bg-white w-full h-16 rounded-t-3xl fixed bottom-0 left-0 right-0">
-      <nav class="grid grid-rows-1 grid-cols-6 gap-5 w-full h-full place-items-center">
-        <a>home</a>
-        <a>crm</a>
-        <a>activity history</a>
-        <a>calendar</a>
-        <a>payments</a>
-        <a>new expense</a>
+    <footer class="max-w-screen-2xl mx-auto bg-white w-full h-16 rounded-t-3xl fixed bottom-0 left-0 right-0 px-4">
+      <nav class="grid grid-rows-1 grid-cols-6 h-full place-items-center text-gray-500">
+        <RouterLink to="/" activeClass="text-black">
+          <HomeIcon class="h-full w-8" />
+        </RouterLink>
+        <RouterLink to="/crm" activeClass="text-black">
+          <PeopleIcon class="h-full w-8" />
+        </RouterLink>
+        <RouterLink to="/activities" activeClass="text-black">
+          <ClockIcon class="h-full w-8" />
+        </RouterLink>
+        <RouterLink to="/calendar" activeClass="text-black">
+          <CalendarIcon class="h-full w-8" />
+        </RouterLink>
+        <RouterLink to="/expenses" activeClass="text-black">
+          <WalletIcon class="h-full w-8" />
+        </RouterLink>
+        <RouterLink to="/expenses/new" activeClass="text-black">
+          <CardIcon class="h-full w-8" />
+        </RouterLink>
       </nav>
     </footer>
   </main>
